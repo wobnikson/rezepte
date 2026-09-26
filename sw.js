c7f3cee71f576152474a7062ac7de77eb1588afb
wobnikson <154746278+wobnikson@users.noreply.github.com>
2026-09-26 12:01:48 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Offline-Speicher. Die Versionsnummer ändert sich bei jeder neuen Fassung.
-const CACHE = 'rezeptordner-9b5d5fe7';
+const CACHE = 'rezeptordner-5550cdc7';
 const FILES = ['./', 'index.html', 'rezepte.js', 'manifest.json', 'icon-192.png', 'icon-512.png', 'apple-touch-icon.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
